da9cefe6f3c03c5276ae93eef43ad2f564724d76
Artem <50289290+ArtemMukhrin@users.noreply.github.com>
2022-02-24 17:06:28 +0300

    Am (#1)
    * main
    
    * second main
    
    * wtf
    
    Co-authored-by: Artem <Artem@yandex.ru>

---
+++ src/main/java/src/SecondClass.java
@@ -0,0 +1,13 @@
+package src;
+
+public class SecondClass {
+
+    public static void main(String[] args) {
+
+        //dfsdfsdklfhjsdjkghvsdfjkhvkjdfhg
+
+        System.out.println("how are you");
+
+    }
+
+}

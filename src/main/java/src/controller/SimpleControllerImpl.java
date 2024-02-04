142835bc0bf9ca388ee0ec921747841a6c7ecfbc
Artem Mukhrin <artem.mukhrin@gazprombank.ru>
2024-02-04 17:47:38 +0300

    GBO-1 methods implementation

---
+++ src/main/java/src/controller/SimpleControllerImpl.java
@@ -0,0 +1,16 @@
+package src.controller;
+
+import java.util.UUID;
+
+/**
+ * SimpleControllerImpl.
+ *
+ * @author art
+ */
+public class SimpleControllerImpl implements SimpleController{
+
+  @Override
+  public String getPage(UUID userId) {
+    return null;
+  }
+}
